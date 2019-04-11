ec9a740219f1221f99dc8d9c5d24509446e56f4f
TimoBrandt1 <41124729+TimoBrandt1@users.noreply.github.com>
2019-04-11 08:34:20 +0200

    Add files via upload

---
+++ Leerjaar 1/Periode 1.3/proj/Platformer/Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 25;
+    private Rigidbody2D rb;
+
+    public float jumpHeight = 400;
+    private bool onGround;
+    void Start()
+    {
+        onGround = true;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && onGround == true)
+        {
+            rb.AddForce(Vector2.up * jumpHeight);
+            onGround = false;
+            Debug.Log(onGround);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= transform.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += transform.right * speed * Time.deltaTime;
+        }
+    }
+
+    
+    private void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.CompareTag("Ground"))
+        {
+            onGround = true;        }
+    }
+}
