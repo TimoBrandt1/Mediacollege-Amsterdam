6537726e237abca0d1177b8bbbeebc0899428511
TimoBrandt1 <41124729+TimoBrandt1@users.noreply.github.com>
2019-04-26 12:13:35 +0200

    Add files via upload

---
+++ Leerjaar 1/Periode 1.3/DIP/Survival Game/Scripts/enemyScripts/tracking.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class tracking : MonoBehaviour
+{
+    public NavMeshAgent agentP;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    public GameObject player;
+    public float speed;
+    private Vector3 startPos;
+
+    // Update is called once per frame
+    void Update()
+    {
+        agentP.SetDestination(player.transform.position);
+    }
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            transform.position = startPos;
+            GameObject.Find("Player").GetComponent<health>().amountOfHearts--;
+        }
+    }
+}
